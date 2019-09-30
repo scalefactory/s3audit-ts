4ce05394e12a9c907ae0fd68da9602fee960036d
Rob Taylor <robbytaylor@users.noreply.github.com>
2019-09-30 17:25:36 +0100

    Added check for bucket public access block

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,114 @@
 import {Command, flags} from '@oclif/command'
+import * as Config from '@oclif/config';
+import {S3} from 'aws-sdk'
+
+const Listr = require('listr')
 
 class S3Audit extends Command {
   static description = 'describe the command here'
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
+  private s3: S3;
+
+  constructor(argv: string[], config: Config.IConfig) {
+    super(argv, config);
+
+    this.s3 = new S3();
+  }
 
   async run() {
-    const {args, flags} = this.parse(S3Audit)
+    const buckets: string[] = []
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    this.s3.listBuckets((error: Object, data?: S3.Types.ListBucketsOutput) => {
+      if (!data || typeof data.Buckets === 'undefined') {
+        return this.exit();
+      }
+
+      data.Buckets.forEach(bucket => {
+        if (typeof bucket.Name !== 'undefined') {
+          buckets.push(bucket.Name);
+        }
+      });
+
+      this.auditBuckets(buckets);
+    })
+  }
+
+  private async auditBuckets(buckets: Array<string>) {
+    const tasks = new Listr([], {
+      exitOnError: false,
+      collapse: false,
+      concurrent: true
+    });
+
+    buckets.forEach(bucketName => {
+      tasks.add([
+        {
+          title: bucketName,
+          task: () => {
+            return new Listr([
+              {
+                title: 'Bucket public access block',
+                task: () => this.checkBucketPublicAccess(bucketName)
+              }
+            ], {concurrent: true});
+          }
+        }
+      ]);
+    });
+
+    tasks.run().catch((err: any) => {});
+  }
+
+  private async checkBucketPublicAccess(bucketName: string) {
+    return new Promise((resolve, reject) => {
+      this.s3.getPublicAccessBlock({Bucket: bucketName}, (error: Object, data: S3.Types.GetPublicAccessBlockOutput) => {
+        if (data === null) {
+          return reject(new Error('Bucket has no public access block'));
+        }
+
+        const publicAccessBlockConfiguration = data.PublicAccessBlockConfiguration || {};
+
+        resolve(new Listr([
+          {
+            title: 'BlockPublicAcls',
+            task: () => {
+              if (publicAccessBlockConfiguration.BlockPublicAcls !== true) {
+                throw new Error();
+              }
+            }
+          },
+          {
+            title: 'IgnorePublicAcls',
+            task: () => {
+              if (publicAccessBlockConfiguration.IgnorePublicAcls !== true) {
+                throw new Error();
+              }
+            }
+          },
+          {
+            title: 'BlockPublicPolicy',
+            task: () => {
+              if (publicAccessBlockConfiguration.BlockPublicPolicy !== true) {
+                throw new Error();
+              }
+            }
+          },
+          {
+            title: 'RestrictPublicBuckets',
+            task: () => {
+              if (publicAccessBlockConfiguration.RestrictPublicBuckets !== true) {
+                throw new Error();
+              }
+            }
+          }
+        ], {concurrent: true}));
+      })
+    })
   }
 }
 
